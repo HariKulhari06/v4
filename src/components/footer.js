4da17c7ef544b3d3fe27f6bb6c76dd2b1ae9eb6f
Hari <hari.choudhary1905@gmail.com>
2022-10-31 14:52:30 +0530

    updated github

--- src/components/footer.js
+++ src/components/footer.js
@@ -77,7 +77,7 @@ const Footer = () => {
     if (process.env.NODE_ENV !== 'production') {
       return;
     }
-    fetch('https://api.github.com/repos/bchiang7/v4')
+    fetch('https://api.github.com/repos/harikulhari06/v4')
       .then(response => response.json())
       .then(json => {
         const { stargazers_count, forks_count } = json;
@@ -105,7 +105,7 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
+        <a href="https://github.com/harikulhari06/v4">
           <div>Designed &amp; Built by Hari Singh Kulhari</div>
 
           {githubInfo.stars && githubInfo.forks && (
